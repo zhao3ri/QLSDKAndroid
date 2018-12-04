a3a797847e2823e509555fa86cad80a1e33e7f6a
tyland <441742980@qq.com>
2018-12-04 11:02:55 +0800

    添加配置第三方参数的方式

--- qlsdk-core/src/main/java/com/qinglan/sdk/android/platform/BasePlatform.java
+++ qlsdk-core/src/main/java/com/qinglan/sdk/android/platform/BasePlatform.java
@@ -1,6 +1,9 @@
 package com.qinglan.sdk.android.platform;
 
+import android.text.TextUtils;
+
 import com.qinglan.sdk.android.Config;
+import com.qinglan.sdk.android.common.ResContainer;
 import com.qinglan.sdk.android.model.UserInfo;
 
 public abstract class BasePlatform implements IPlatform {
@@ -8,12 +11,25 @@ public abstract class BasePlatform implements IPlatform {
     private String platformName;
     protected Config gameConfig;
     protected UserInfo user;
+    private static final String RES_NAME_APP_ID = "qlsdk_third_party_appid";
+    private static final String RES_NAME_APP_KEY = "qlsdk_third_party_appkey";
+    private static final String RES_NAME_PRIVATE_KEY = "qlsdk_third_party_private";
+
 
     @Override
     public void load(PlatformParamsReader.PlatformParam p, Config config) {
         platformId = p.id;
         platformName = p.name;
         gameConfig = config;
+        if (TextUtils.isEmpty(gameConfig.getAppID())) {
+            gameConfig.setAppID(ResContainer.getString(config.getContext(), RES_NAME_APP_ID));
+        }
+        if (TextUtils.isEmpty(gameConfig.getAppKey())) {
+            gameConfig.setAppKey(ResContainer.getString(config.getContext(), RES_NAME_APP_KEY));
+        }
+        if (TextUtils.isEmpty(gameConfig.getPrivateKey())) {
+            gameConfig.setPrivateKey(ResContainer.getString(config.getContext(), RES_NAME_PRIVATE_KEY));
+        }
     }
 
     @Override
